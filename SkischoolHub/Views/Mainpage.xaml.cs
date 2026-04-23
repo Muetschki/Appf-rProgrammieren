583a66e74275412bda45f43dfd2addef9c08b33d
Muetschki <xmahlknecht@tsn.at>
2026-04-23 08:04:21 +0200

    Stand 23.04.

--- SkischoolHub/Views/Mainpage.xaml.cs
+++ SkischoolHub/Views/Mainpage.xaml.cs
@@ -9,4 +9,9 @@ public partial class MainPage : ContentPage
         InitializeComponent();
         BindingContext = vm;
     }
+
+    private async void OnLogoutClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//LoginPage");
+    }
 }
